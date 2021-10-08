d7cfcfc6cbbc6c8517b732f6b3a621ce1bcbcec1
Paula Saavedra <paula.saavedraca@amigo.edu.co>
2021-10-07 23:57:47 -0500

    primer componente

--- appventas/src/routes/App.js
+++ appventas/src/routes/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 function App () {
   return (
-    
+    <div className='container-fluid'>
+      <Sidebar/>
+
+    </div>
   );
 }
 
