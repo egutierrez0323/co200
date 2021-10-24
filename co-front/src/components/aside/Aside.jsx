3daa9bf372fb4990abd3250ca564c9866403dd32
Emmanuel Gutierrez <gutierrezgarciaem@gmail.com>
2021-10-23 20:09:45 -0500

    se soluciona conflicto

--- co-front/src/components/aside/Aside.jsx
+++ co-front/src/components/aside/Aside.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 
 function Aside() {
@@ -59,65 +58,3 @@ function Aside() {
 }
 
 export default Aside;
-=======
-import { Link } from "react-router-dom";
-
-function Aside() {
-  return (
-    <div className="sidebar" data-color="black" data-active-color="react">
-      <div className="logo">
-        <a
-          href="/"
-          className="simple-text logo-mini"
-        >
-          <div className="logo-image-small">
-            <i className="fab fa-react fa-2x"></i>
-          </div>
-        </a>
-        <a href="" className="simple-text logo-normal">
-          App Ventas co200
-        </a>
-      </div>
-      <div className="sidebar-wrapper">
-        <ul className="nav">
-          <li className="active ">
-            <Link to="/home">
-              <i className="fas fa-home"></i>
-              <p>Dashboard</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/ventas">
-              <i className="fab fa-accessible-icon"></i>
-              <p>Gestion Ventas</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/productos">
-              <i className="fas fa-boxes"></i>
-              <p>Gestion Productos</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="./usuarios">
-              <i className="fas fa-user-cog"></i>
-              <p>Gestion Usuarios</p>
-            </Link>
-          </li>
-          <li>
-            <a href="./user.html">
-              <i className="far fa-user"></i>
-              <p>User Profile</p>
-            </a>
-          </li>
-          <li>
-            
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export default Aside;
->>>>>>> fae88100b07d3bdb0a163a411e095dbb2e615ad5
