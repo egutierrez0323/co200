dae29f37ae1c6a4bc1ecbf48a46f28f7dbb1962d
Emmanuel Gutierrez <gutierrezgarciaem@gmail.com>
2021-10-27 20:36:24 -0500

    Modificacion en las tablas de Menus

--- co-front/src/routes/Routes.js
+++ co-front/src/routes/Routes.js
@@ -3,7 +3,7 @@ import { Redirect } from "react-router";
 import Login from "../components/Login/Login";
 import Home from "../pages/home/Home";
 import Ventas from "../pages/gestionVentas/Gestion_Ventas";
-import Productos from "../pages/gestionProductos/GestionProductos";
+import Productos from "../pages/gestionProductos/GestionProductos.jsx";
 import Usuarios from "../pages/gestionUsuarios/GestionUsuarios";
 
 function Routes() {
