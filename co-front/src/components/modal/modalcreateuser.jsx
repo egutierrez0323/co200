dab147af30b230222b7963fd0d646277372f4edf
ciroac3188 <68131419+ciroac3188@users.noreply.github.com>
2021-11-03 01:31:00 -0500

    conexión front-end y back-end

--- co-front/src/components/modal/modalcreateuser.jsx
+++ co-front/src/components/modal/modalcreateuser.jsx
@@ -1,26 +1,28 @@
 import React,{useState} from 'react';
 import {Form, Modal} from 'react-bootstrap';
 import DefaultButtom from '../defaultButton/defaultButtom';
-import Inputform from '../../components/inputform/inputform';
+import Inputform from '../../components/inputform/inputform2';
 
 const ModalcreateUser = ({show,handleClose, onSend}) => {
 
-    const [nombreform, setNombreform] = useState('');
-    const [telefonoform, setTelefonoform] = useState('');
-    const [emailform, setEmailform] = useState('');
-    const [rolform, setRolform] = useState('');
-    const [estadoform, setEstadoform] = useState('');
+    const [nombreform, setNombreform] = useState("");
+    const [telefonoform, setTelefonoform] = useState("");
+    const [emailform, setEmailform] = useState("");
+    const [rolform, setRolform] = useState("");
+    const [estadoform, setEstadoform] = useState("");
 
-    function senData(){
+    const sendData = () => {
         const json = {
             nombre : nombreform,
             telefono : telefonoform,
-            email : emailform,
             rol : rolform,
+            email : emailform,
+            password : "default",
             estado : estadoform
         };
-        onSend(json)
-        cleardata()
+         console.log(nombreform+telefonoform);
+        onSend(json);
+        cleardata();
     }
 
     function cleardata(){
@@ -40,15 +42,14 @@ const ModalcreateUser = ({show,handleClose, onSend}) => {
                     <Modal.Title>Crear Usuario</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                {/* {AQUI IRA EL FORMULARIO} */}
-                    <Inputform value={nombreform} defaultText={"Nombre"} type={1} onChange={(e) => setNombreform(e.target.value)}/>
-                    <Inputform value={telefonoform} defaultText={"Telefono"} type={1} onChange={(e) => setTelefonoform(e.target.value)}/>
-                    <Inputform value={emailform} defaultText={"Email"} type={1} onChange={(e) => setEmailform(e.target.value)}/>
-                    <Inputform value={rolform} defaultText={"Rol"} type={1} onChange={(e) => setRolform(e.target.value)}/>
-                    <Inputform value={estadoform} defaultText={"Estado"} type={1} onChange={(e) => setEstadoform(e.target.value)}/>
+                    <Inputform text="Nombre Completo:" value={nombreform} defaultText={"Nombre"} type={1} onChange={(e) => setNombreform(e.target.value)}/>
+                    <Inputform text="Teléfono:" value={telefonoform} defaultText={"Telefono"} type={1} onChange={(e) => setTelefonoform(e.target.value)}/>
+                    <Inputform text="Email:" value={emailform} defaultText={"Email"} type={1} onChange={(e) => setEmailform(e.target.value)}/>
+                    <Inputform text="Rol:" value={rolform} defaultText={"Rol"} type={1} onChange={(e) => setRolform(e.target.value)}/>
+                    <Inputform text="Estado:" value={estadoform} defaultText={"Estado"} type={1} onChange={(e) => setEstadoform(e.target.value)}/>
                 </Modal.Body>
                 <Modal.Footer>
-                    <DefaultButtom typebuttom={1} text={"Registrar Usuario"} onClick={onSend ? senData : undefined}  />
+                    <DefaultButtom typebuttom={1} text={"Registrar Usuario"} onClick={sendData}/>
                     <DefaultButtom typebuttom={2} text={"Close"} onClick={handleClose} />
                 </Modal.Footer>
             </Form> 
